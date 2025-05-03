f685a90fa7bad8be0e1f4faa6d396f5391bd1a43
Sai Krishna Resu <143726106+saikrishnaresuk@users.noreply.github.com>
2025-05-03 16:54:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_launch_template" "web_server_as" {
      security_groups = [aws_security_group.web_server.id]
      subnets = ["subnet-078f3e87189aa9788", "subnet-055b9cf542a598c27"]
      listener {
-      instance_port     = 8000
+      instance_port     = 80
       instance_protocol = "http"
       lb_port           = 80
       lb_protocol       = "http"
