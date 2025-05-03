6f914a6ff1f5054bcc5adfc44b6f64dd60570d78
Sai Krishna Resu <143726106+saikrishnaresuk@users.noreply.github.com>
2025-05-03 14:01:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0454e52560c7f5c55"
+    image_id           = "ami-085386e29e44dacd7"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "lastone"
+    key_name = "SAIKRISHNA_SG"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-024286466e258f339", "subnet-09558309acf4661a8"]
+     subnets = ["subnet-078f3e87189aa9788", "subnet-055b9cf542a598c27"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
